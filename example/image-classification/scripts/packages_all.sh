2d35ec2308e3efb0743d28216c14e8d8b4e170d3
Carl Yang <ctcyang@ucdavis.edu>
2018-06-28 21:37:57 +0000

    blah

--- example/image-classification/scripts/packages_all.sh
+++
@@ -1,9 +0,0 @@
-cat hosts | while read host;
-do 
-  echo $host
-  scp packages.sh ubuntu@$host:/home/ubuntu
-  scp /home/ubuntu/nccl-repo-ubuntu1604-2.1.15-ga-cuda9.0_1-1_amd64.deb ubuntu@$host:/home/ubuntu
-  ssh -o "StrictHostKeyChecking no" $host 'sh packages.sh' &
-done
-sh packages.sh
-wait
